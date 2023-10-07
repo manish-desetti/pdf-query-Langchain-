89e8f959db3b71912de2cba048e90fa1e5e80a52
manish <manishdesetti@gmail.com>
2023-10-07 05:51:06 +0530

    final app

--- app.py
+++ app.py
@@ -12,13 +12,60 @@ from langchain.callbacks import get_openai_callback
 import os
 
 with st.sidebar:
-    st.title('🤗💬 LLM Chat App')
+    st.title('🤗💬 PDF Chat App')
     st.markdown('''
     ## About
     This app is an LLM-powered chatbot built using:
     - [Streamlit](https://streamlit.io/)
     - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+    - [OpenAI](https://platform.openai.com/docs/models) 
  
     ''')
+load_dotenv()  
+def main():
+    st.header("Chat with PDF 💬")
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
     
+    #concatenate all the pages of pdf and convert them into chunks
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunks = text_splitter.split_text(text=text)
+        
+        #if a new pdf is uploaded create embeddings and store it on disk else open the stored file on disk and it's embeddings
+        store_name = pdf.name[:-4]
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+            #st.write('Embeddings Loaded from the Disk')
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+            #st.write('New Embeddings Created')    
+        
+        #get ranked results-3 most similar docs
+        query = st.text_input("Ask questions about your PDF file:")
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+            #st.write(docs)
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+
+
+
+if __name__ == '__main__':
+    main()
